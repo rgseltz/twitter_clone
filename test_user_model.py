1d8dcb068129a2d17f5b49808d98ca489745aac7
ryan <ryanjgecelter@gmail.com>
2022-06-07 10:14:31 +0300

    tested user model for functionality

--- test_user_model.py
+++ test_user_model.py
@@ -4,6 +4,8 @@
 #
 #    python -m unittest test_user_model.py
 
+from app import app
+from plistlib import UID
 
 import os
 from unittest import TestCase
@@ -15,17 +17,13 @@ from models import db, User, Message, Follows
 # before we import our app, since that will have already
 # connected to the database
 
-os.environ['DATABASE_URL'] = "postgresql:///warbler-test"
+os.environ['DATABASE_URL'] = "postgresql:///warbler_test"
 
 
 # Now we can import app
-
-from app import app
-
 # Create our tables (we do this here, so we only create the tables
 # once for all tests --- in each test, we'll delete the data
 # and create fresh new clean test data
-
 db.create_all()
 
 
@@ -38,6 +36,23 @@ class UserModelTestCase(TestCase):
         User.query.delete()
         Message.query.delete()
         Follows.query.delete()
+
+        u1 = User.signup('user1', 'user1@email.com', 'password', None)
+        u1id = 1
+        u1.id = u1id
+
+        u2 = User.signup('user2', 'user2@email.com', 'password2', None)
+        u2id = id
+
+        db.session.add(u1)
+        db.session.add(u2)
+        db.session.commit()
+
+        self.u1 = u1
+        self.u1id = u1id
+
+        self.u2 = u2
+        self.u2id = u2id
 
         self.client = app.test_client()
 
@@ -53,6 +68,42 @@ class UserModelTestCase(TestCase):
         db.session.add(u)
         db.session.commit()
 
+        self.u = u
+
         # User should have no messages & no followers
         self.assertEqual(len(u.messages), 0)
         self.assertEqual(len(u.followers), 0)
+
+    def test_user_follows(self):
+        self.u1.following.append(self.u2)
+
+        self.assertEqual(len(self.u1.following), 1)
+        self.assertEqual(len(self.u2.following), 0)
+        self.assertEqual(self.u1.following[0], self.u2)
+
+        self.u2.following.append(self.u1)
+        self.assertEqual(self.u2.following[0], self.u1)
+
+    def test_user_is_following(self):
+        self.u1.following.append(self.u2)
+
+        self.assertEqual(self.u1.is_following(self.u2), True)
+        self.assertEqual(self.u1.is_followed_by(self.u2), False)
+        self.assertEqual(self.u2.is_followed_by(self.u1), True)
+
+        self.u2.following.append(self.u1)
+        self.assertEqual(self.u2.is_following(self.u1), True)
+        self.assertEqual(self.u1.is_following(self.u2), True)
+
+    def test_new_user(self):
+        u_test = User.signup(
+            'usertest1', 'usertest1@test.com', 'testuser1password', None)
+        id = 10
+        u_test.id = id
+        db.session.add(u_test)
+        db.session.commit()
+        u_test = User.query.get(u_test.id)
+
+        self.assertEqual(u_test.username, 'usertest1')
+        self.assertEqual(self.u2.username, 'user2')
+        self.assertEqual(u_test.email, 'usertest1@test.com')
